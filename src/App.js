0bd5474be62fb11fd3322520d31fe39e66037e2a
Igah Emmanuel <Igah.emmanuel@gmail.com>
2024-07-18 11:16:35 +0100

    Add main content and footer with 100vh height for body

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 // src/App.js
 import React from 'react';
 import Navbar from './components/Navbar';
+import './styles.css'; // Import the CSS file
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <div className="main-content">
       {/* Other components or content */}
+      <h1>This is my website!</h1>
+        <p>This website is my project given to me by Alex.</p>
+      </div>
+      <footer>
+        <p>&copy; 2024 ibankings. All rights reserved.</p>
+      </footer>
     </div>
   );
 }
